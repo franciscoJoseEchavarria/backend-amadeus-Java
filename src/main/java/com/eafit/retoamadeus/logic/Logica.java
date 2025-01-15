65c1f94798c8cc2a006a7484e9b343830c0d2380
Francisco Echavarría <fjepaez@gmail.com>
2025-01-15 18:59:36 -0500

    resolviendo lógica de negocio

---
+++ src/main/java/com/eafit/retoamadeus/logic/Logica.java
@@ -0,0 +1,29 @@
+package com.eafit.retoamadeus.logic;
+
+import com.eafit.retoamadeus.entities.UserQueryEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //getters, setters, equals, hashcode, tostring
+@NoArgsConstructor //constructor sin argumentos
+@AllArgsConstructor //constructor con todos los argumentos
+@Builder //patron de diseño builder
+
+
+public class Logica {
+
+    private String destinoAmerica;
+    private String destinoEuropa;
+
+    public logicaNegocio (UserQueryEntity userQueryEntity){
+
+        switch (environmentType1 = "playa" && climateType2= "caliuros" &&
+                accommodationType3 = "hotel de lujo" && activityType4 = "" )
+    }
+
+
+
+
+}
